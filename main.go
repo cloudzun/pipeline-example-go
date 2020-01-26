0b9cd78dd4b0f65d2744ee974a8bf1390e7f88ff
CloudZun <info@cloudzun.com>
2020-01-26 15:18:32 +0800

    Update main.go

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 	"log"
 )
 
-const webContent = "Hello World! Abraham was here 2020-1-24 22:11:11.02"
+const webContent = "Hello World! Abraham was here  01/26/2020 Sun 15:18:19.31"
 
 func main() {
 	http.HandleFunc("/", helloHandler)
